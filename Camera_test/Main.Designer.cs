a333aa03f9f210bc984ddacfefa06ccc577028d2
c13proto <c13proto@aqua.ocn.ne.jp>
2016-04-15 13:56:02 +0900

    lw115mのテスト
    基本usb接続のカメラでいける

---
+++ Camera_test/Main.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Camera_test
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_width = new System.Windows.Forms.TextBox();
+            this.textBox_height = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_FPS = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_width
+            // 
+            this.textBox_width.Location = new System.Drawing.Point(44, 10);
+            this.textBox_width.Name = "textBox_width";
+            this.textBox_width.Size = new System.Drawing.Size(37, 19);
+            this.textBox_width.TabIndex = 0;
+            this.textBox_width.Text = "640";
+            // 
+            // textBox_height
+            // 
+            this.textBox_height.Location = new System.Drawing.Point(44, 35);
+            this.textBox_height.Name = "textBox_height";
+            this.textBox_height.Size = new System.Drawing.Size(37, 19);
+            this.textBox_height.TabIndex = 1;
+            this.textBox_height.Text = "480";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(9, 85);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(72, 23);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.Click_start);
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(100, 10);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(516, 363);
+            this.pictureBoxIpl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxIpl1.TabIndex = 3;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "FPS";
+            // 
+            // textBox_FPS
+            // 
+            this.textBox_FPS.Location = new System.Drawing.Point(44, 60);
+            this.textBox_FPS.Name = "textBox_FPS";
+            this.textBox_FPS.Size = new System.Drawing.Size(37, 19);
+            this.textBox_FPS.TabIndex = 6;
+            this.textBox_FPS.Text = "30";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(626, 385);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_FPS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox_height);
+            this.Controls.Add(this.textBox_width);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_width;
+        private System.Windows.Forms.TextBox textBox_height;
+        private System.Windows.Forms.Button button_start;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_FPS;
+    }
+}
+
---
+++ Camera_test/Main.cs
@@ -0,0 +1,87 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Camera_test
+{
+    public partial class Main : Form
+    {
+        bool STREAM = false;
+        CvCapture CAPTURE;
+        private System.Timers.Timer timer_back;
+        private System.Windows.Forms.Timer timer_form;
+        public static IplImage camera;
+
+
+        public Main()
+        {
+            InitializeComponent();
+            CAPTURE = Cv.CreateCameraCapture(0);
+        }
+
+        private void Click_start(object sender, EventArgs e)
+        {
+            STREAM = !STREAM;
+            if (STREAM)
+            {
+                カメラ設定(int.Parse(textBox_width.Text), int.Parse(textBox_height.Text), int.Parse(textBox_FPS.Text));
+                タイマー開始();
+                button_start.Text = "stop";
+            }
+            else
+            {
+                タイマー停止();
+                button_start.Text = "start";
+            }
+        }
+
+        void カメラ設定(int w, int h,int fps)
+        {
+            Cv.SetCaptureProperty(CAPTURE, CaptureProperty.FrameWidth, w);
+            Cv.SetCaptureProperty(CAPTURE, CaptureProperty.FrameHeight, h);
+            CAPTURE.Fps = fps;
+        }
+        private void form_ctrl(object sender, EventArgs e)//タイマ割り込みで行う処理
+        {
+            pictureBoxIpl1.RefreshIplImage(camera);
+        }
+        void back_ctrl(object sender, ElapsedEventArgs e)
+        {
+            var frame = Cv.QueryFrame(CAPTURE);
+            if (frame != null)camera = frame.Clone();
+            else System.Diagnostics.Debug.WriteLine("frame=null");
+            Cv.ReleaseImage(frame);
+        }
+
+        void タイマー開始()
+        {
+            if (timer_form != null) timer_form.Dispose();
+            timer_form = new System.Windows.Forms.Timer();
+            timer_form.Tick += new EventHandler(form_ctrl);
+            timer_form.Interval = 1000/int.Parse(textBox_FPS.Text);
+            timer_form.Start();
+
+            if (timer_back != null) timer_back.Dispose();
+            timer_back = new System.Timers.Timer();
+            timer_back.Elapsed += new ElapsedEventHandler(back_ctrl);
+            timer_back.Interval = 1000/int.Parse(textBox_FPS.Text);
+            timer_back.Start();
+        }
+        void タイマー停止()
+        {
+            timer_form.Stop();
+            timer_form.Dispose();
+
+            timer_back.Stop();
+            timer_back.Dispose();
+        }
+    }
+}
