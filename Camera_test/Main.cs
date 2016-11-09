29a38462511042099111c9be1632ce1194c1fc2b
c13proto <c13proto@aqua.ocn.ne.jp>
2016-11-09 15:11:49 +0900

    opencv3に変更

--- Camera_test/Main.cs
+++ Camera_test/Main.cs
@@ -15,16 +15,19 @@ namespace Camera_test
     public partial class Main : Form
     {
         bool STREAM = false;
-        CvCapture CAPTURE;
+        VideoCapture CAPTURE;
+        //VideoWriter Writer;
+        Mat FRAME;
         private System.Timers.Timer timer_back;
         private System.Windows.Forms.Timer timer_form;
-        public static IplImage camera;
+        
 
 
         public Main()
         {
             InitializeComponent();
-            CAPTURE = Cv.CreateCameraCapture(0);
+            CAPTURE = new VideoCapture(0);
+            //Writer = new VideoWriter();
         }
 
         private void Click_start(object sender, EventArgs e)
@@ -45,25 +48,31 @@ namespace Camera_test
 
         void カメラ設定(int w, int h,int fps)
         {
-            Cv.SetCaptureProperty(CAPTURE, CaptureProperty.FrameWidth, w);
-            Cv.SetCaptureProperty(CAPTURE, CaptureProperty.FrameHeight, h);
-            CAPTURE.Fps = fps;
+            CAPTURE.Set(CaptureProperty.FrameHeight,h);
+            CAPTURE.Set(CaptureProperty.FrameWidth, w);
+            CAPTURE.Set(CaptureProperty.Fps,fps);
 
-            if (camera != null) camera.Dispose();
-            camera = Cv.CreateImage(new CvSize(w, h), BitDepth.U8, 3);//カラーらしい
-            camera.Zero();
-
+            if (FRAME != null) FRAME.Dispose();
+            FRAME = new Mat(CAPTURE.FrameHeight, CAPTURE.FrameWidth, MatType.CV_8UC3);//カラーらしい
+            textBox_width.Text = CAPTURE.FrameWidth.ToString();//設定できたフレームの大きさを表示
+            textBox_height.Text = CAPTURE.FrameHeight.ToString();
+            textBox_FPS.Text = ((int)(CAPTURE.Fps)).ToString();
         }
         private void form_ctrl(object sender, EventArgs e)//タイマ割り込みで行う処理
         {
-            pictureBoxIpl1.RefreshIplImage(camera);
+            //var mat3 = new MatOfByte3(FRAME);
+            //var indexer = mat3.GetIndexer();
+            var color = new MatOfByte3(FRAME).GetIndexer()[0, 0];//(y,x)の画素を取得
+            pictureBoxIpl1.RefreshIplImage(FRAME);
+            Console.WriteLine("({0},{1},{2})", color[0], color[1], color[2]);
         }
         void back_ctrl(object sender, ElapsedEventArgs e)
         {
-            var frame = Cv.QueryFrame(CAPTURE);
-            if (frame != null) Cv.Copy(frame,camera);
-            else System.Diagnostics.Debug.WriteLine("frame=null");
-            Cv.ReleaseImage(frame);
+            var frame = new Mat(FRAME.Height,FRAME.Width,FRAME.Type());
+            var isFrame=CAPTURE.Read(frame);
+            if(!isFrame)System.Diagnostics.Debug.WriteLine("frame=null");
+            frame.CopyTo(FRAME);
+            frame.Release();
         }
 
         void タイマー開始()
@@ -71,13 +80,13 @@ namespace Camera_test
             if (timer_form != null) timer_form.Dispose();
             timer_form = new System.Windows.Forms.Timer();
             timer_form.Tick += new EventHandler(form_ctrl);
-            timer_form.Interval = 1000/int.Parse(textBox_FPS.Text);
+            timer_form.Interval = (int)(1000/double.Parse(textBox_FPS.Text));
             timer_form.Start();
 
             if (timer_back != null) timer_back.Dispose();
             timer_back = new System.Timers.Timer();
             timer_back.Elapsed += new ElapsedEventHandler(back_ctrl);
-            timer_back.Interval = 1000/int.Parse(textBox_FPS.Text);
+            timer_back.Interval = (int)(1000 / double.Parse(textBox_FPS.Text));
             timer_back.Start();
         }
         void タイマー停止()
